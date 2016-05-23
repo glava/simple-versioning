dbca1e85fc7658d58798d146947beffee73e5568
Goran Ojkić <goran.ojkic@fyber.com>
2016-05-23 15:34:34 +0200

    Simpler is better

--- project/Release.scala
+++ project/Release.scala
@@ -4,37 +4,17 @@ import sbtrelease.ReleasePlugin.autoImport._
 import sbtrelease.ReleaseStateTransformations._
 
 object Release {
+
   lazy val mergeDevelop = ReleaseStep(action = st => {
-    // extract the build state
     val extracted = Project.extract(st)
 
     val git = extracted.get(releaseVcs).get.asInstanceOf[Git]
-    val curBranch = (git.cmd("rev-parse", "--abbrev-ref", "HEAD") !!).trim
     git.cmd("checkout", "develop") ! st.log
     git.cmd("pull", "origin", "develop") ! st.log
     git.cmd("merge", "master") ! st.log
-    git.cmd("push", "origin", "master") ! st.log
+    git.cmd("push", "origin", "develop") ! st.log
     git.cmd("checkout", "master") ! st.log
     st.log.info("Develop merged with master")
-    st
-  })
-
-  lazy val masterOnly = ReleaseStep(action = st => {
-    val extracted = Project.extract(st)
-
-    val git = extracted.get(releaseVcs).get.asInstanceOf[Git]
-    val curBranch = (git.cmd("rev-parse", "--abbrev-ref", "HEAD") !!).trim
-    if (curBranch != "master") throw new IllegalArgumentException("Releases are available from master branch")
-    st
-  })
-
-  val checkOrganization = ReleaseStep(action = st => {
-    // extract the build state
-    import sbt._
-    val extracted = Project.extract(st)
-
-    val file: File = new File(extracted.currentProject.base.absolutePath + "/version.sbt")
-    st.log.info(file.absolutePath)
     st
   })
 
@@ -45,13 +25,13 @@ object Release {
     tagRelease,
     setNextVersion,
     commitNextVersion,
-    pushChanges
+    pushChanges,
+    mergeDevelop
   )
-
 
   def assemblyVersion(version: String, headCommit: Option[String]) = {
     headCommit match {
-      case Some(hash) if version.endsWith("-SNAPSHOT") => s"$version-${hash.take(6)}"
+      case Some(hash) if version.endsWith("-SNAPSHOT") => s"$version-$hash"
       case _ => version
     }
   }
